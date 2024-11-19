14b98fda7ab87e4ea7065f69cf2a23dcc13dddea
bliu9 <bryan.liu@menloschool.org>
2024-11-19 14:59:51 -0800

    created the main class and started implementing some more functions in the game class.

--- src/Game.java
+++ src/Game.java
@@ -7,6 +7,39 @@ public class Game
 
     public Game()
     {
+
+    }
+
+    public String[] getNames()
+    {
+        String[] names = new String[2];
         Scanner input = new Scanner(System.in);
+
+        System.out.println("Player 1, what is your name?");
+        names[0] = input.nextLine();
+
+        System.out.println("Player 2, what is your name?");
+        names[1] = input.nextLine();
+
+        return names;
+    }
+
+    public void printRules()
+    {
+        System.out.println(" da rules ");
     }
 }
+
+
+
+/*
+ Card game: uno
+
+ To do:
+ - print rules
+ - get and set player names
+ - start game
+ - play game
+
+
+*/
